2d975b4e24a0416c7069b1a0a5f0cbff5791f6b2
Rahul M <rahul.mutyalkar@gebbs.com>
2020-04-25 06:04:28 +0530

    change production build flow

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'iARWebsite'`, async(() => {
+  it(`should have as title 'DMS'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('iARWebsite');
+    expect(app.title).toEqual('DMS');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to iARWebsite!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DMS!');
   }));
 });
